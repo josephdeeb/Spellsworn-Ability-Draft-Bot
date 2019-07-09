fe72ff29d4420a28e8397cd8335fee42460c117f
Joseph <josephdbesenski@gmail.com>
2019-07-09 17:29:36 -0600

    Worked on bot

--- spellswornAbilityDraft/bot.py
+++ spellswornAbilityDraft/bot.py
@@ -1,19 +1,60 @@
 import discord
+import spellData
+from enum import Enum
 
 client = discord.Client()
+
+class SpellPack:
+    def __init__(self, spells=[]):
+        self.spells = spells
+
+class DraftMember:
+    def __init__(self, player):
+        # discord.member this DraftMember represents
+        self.player = player
+        # Spells this DraftMember has drafted
+        self.pool = []
+        # Pack this DraftMember is currently drafting
+        self.pack = -1
+
+
+"""
+Two major ways to do the drafts:
+    1) The amount of a single spell you draft is the amount you're allowed to level it up
+        i.e. If you draft two magic missile spells, you can level magic missile up to level 2 but not level 3
+    2) When you draft a spell, you can level it as many times as you want
+        A) No multiples of spells at all
+        B) No multiples of spells in singular packs, but separate packs can contain the same spell
+        C) Packs can contain multiples of any spells
+
+    For the time being I'm going to assume 2) B)
+"""
+class DraftGroup:
+    defaultSpellSlots = []
+
+    def __init__(self, id, players):
+        # Identifier of the draft group
+        self.id = id
+        # All discord.members in the draft group
+        self.players = players
+        # All the packs being passed around
+        self.packs = []
+
+    # Initialize all the packs for each player
+    def initializePacks(self, spellSlots):
+
+        # For each player
+        for i in range(len(self.players)):
+
+
+
+# Dictionary that contains all users who have either sent or been mentioned in a draft command.
+# userID:(playingStatus(bool), DraftGroupID)
+userStatuses = {}
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
-def interpretNames(message):
-    # To get to this point, message must begin with "s! draft ", and it must've come from a text channel
-    # Now we want it to include names of discord users in the server from which the message came
-    users = message.content[9:].split(" @")
-    channelMembers = message.channel.members()
-    if len(users) >= 1:
-        for i in range(len(users)):
-            if channelMembers[i].
 
 @client.event
 async def on_message(message):
@@ -21,6 +62,4 @@ async def on_message(message):
         if message.content[3:9].lower() == "draft ":
             #Haven't tested if this is always true even if its a DMChannel
             if type(message.channel) == discord.TextChannel:
-                users = interpretNames(message)
-                if users == False:
-                    # names supplied <= 1
+                users = message.mentions + [message.author]
---
+++ spellswornAbilityDraft/data.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+SpellTypes = Enum('SpellTypes', 'Offensive Defensive Area Utility Travel')
+
+class Spell:
+    def __init__(self, name, type)
+        self.name = name
+        self.type = type
+
+allSpells = [
+    Spell('Bouncer', SpellTypes.Offensive),
+    Spell('Chakram', SpellTypes.Offensive),
+    Spell('Crescent', SpellTypes.Offensive),
+    Spell('Fireball', SpellTypes.Offensive),
+    Spell('Frostbolt', SpellTypes.Offensive),
+    Spell('Homing', SpellTypes.Offensive),
+    Spell('Magic Missile', SpellTypes.Offensive),
+    Spell('Shock', SpellTypes.Offensive),
+
+    Spell('Counter Pulse', SpellTypes.Defensive),
+    Spell('Energy Shield', SpellTypes.Defensive),
+    Spell('Lockdown', SpellTypes.Defensive),
+    Spell('Phase Shift', SpellTypes.Defensive),
+    Spell('Rock Pillar', SpellTypes.Defensive),
+    Spell('Time Anchor', SpellTypes.Defensive),
+    Spell('Windwalk', SpellTypes.Defensive),
+    Spell('Consuming Void', SpellTypes.Defensive),
+
+    Spell('Gravity', SpellTypes.Area),
+    Spell('Lightning Strike', SpellTypes.Area),
+    Spell('Meteor', SpellTypes.Area),
+    Spell('Acid Pool', SpellTypes.Area),
+    Spell('Arcane Mines', SpellTypes.Area),
+
+    Spell('Chain Hook', SpellTypes.Utility),
+    Spell('Dimension Gate', SpellTypes.Utility),
+    Spell('Speed Boost', SpellTypes.Utility),
+    Spell('Fetch', SpellTypes.Utility),
+    Spell('Time Zone', SpellTypes.Utility),
+
+    Spell('Blink', SpellTypes.Travel),
+    Spell('Charge', SpellTypes.Travel),
+    Spell('Swap Ball', SpellTypes.Travel),
+    Spell('Side Step', SpellTypes.Travel)
+]
